87690bed2f861180881fcb615ede69f9a267bb5a
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-11 16:41:49 -0300

    Adiciona funcionalidade de obtenção de produtos na API
    Foram adicionadas novas classes e namespaces para a funcionalidade
    de obtenção de produtos na API de catálogo.
    
    - No arquivo `GetProductsEndPoint.cs`, foi criado o namespace
      `Catalog.API.Products.GetProduct` e a classe `GetProductsEndPoint`.
    - No arquivo `GetProductsHandler.cs`, foi criado o namespace
      `Catalog.API.Products.GetProduct` e adicionadas as seguintes
      classes e registros:
      * `GetProductQuery`, que implementa a interface `IQuery<GetProductsResult>`.
      * `GetProductsResult`, que contém uma lista de produtos
        (`IEnumerable<Product>`).
      * `GetProductsHandler`, que implementa a interface
        `IQueryHandler<GetProductQuery, GetProductsResult>`. Esta classe
        possui um método `Handle` que lida com a consulta de produtos
        (`GetProductQuery`) e retorna o resultado (`GetProductsResult`)
        após consultar a sessão de documentos (`IDocumentSession`).

---
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProduct
+{
+    public record GetProductQuery(): IQuery<GetProductsResult>;
+
+    public record GetProductsResult(IEnumerable<Product> Products);
+    internal class GetProductsHandler (IDocumentSession session, ILogger<GetProductsHandler> logger)
+        : IQueryHandler<GetProductQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsHandler: Handling GetProductQuery {@Query}",query);
+
+            var products = await session
+                .Query<Product>()
+                .ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
