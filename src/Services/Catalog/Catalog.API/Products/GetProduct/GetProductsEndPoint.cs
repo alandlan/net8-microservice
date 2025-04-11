a71be62a72d8043c2b8adb7eb0ec51189b1bde6a
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-11 16:53:30 -0300

    Refatora endpoint de produtos e adiciona documentação
    - O namespace foi alterado de `Catalog.API.Products.GetProduct` para `Catalog.API.Products.GetProduct`.
    - A classe `GetProductsEndPoint` foi substituída por um registro chamado `GetProductsReponse` que contém uma coleção de produtos (`IEnumerable<Product>`).
    - A nova classe `GetProductsEndPoint` agora implementa a interface `ICarterModule`.
    - Implementação do método `AddRoutes` na classe `GetProductsEndPoint`, definindo uma rota GET para "/products".
    - A rota GET utiliza um `ISender` para enviar uma consulta (`GetProductQuery`) e adapta o resultado para um `GetProductsReponse`.
    - A rota GET retorna status 200 OK com o `GetProductsReponse` em caso de sucesso, e também pode retornar status 400 BadRequest e 500 InternalServerError em caso de problemas.
    - A rota GET foi configurada com um resumo e uma descrição para melhor documentação.

--- src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsEndPoint.cs
@@ -1,6 +1,25 @@
-﻿namespace Catalog.API.Products.GetProduct
+﻿
+namespace Catalog.API.Products.GetProduct
 {
-    public class GetProductsEndPoint
+    public record GetProductsReponse(IEnumerable<Product> Products);
+
+    public class GetProductsEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender, CancellationToken cancellationToken) =>
+            {
+                var result = await sender.Send(new GetProductQuery());
+
+                var response = result.Adapt<GetProductsReponse>();
+
+                return Results.Ok(response);
+            }).WithName("GetProducts")
+              .Produces<GetProductsReponse>(StatusCodes.Status200OK)
+              .ProducesProblem(StatusCodes.Status400BadRequest)
+              .ProducesProblem(StatusCodes.Status500InternalServerError)
+              .WithSummary("Get Products")
+              .WithDescription("Get all products from the catalog");
+        }
     }
 }
