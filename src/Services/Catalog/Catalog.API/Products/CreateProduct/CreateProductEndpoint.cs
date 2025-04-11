f4e4646fe4049031a432582d6f803b040ca597fe
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-11 14:56:30 -0300

    Adiciona Carter e Mapster; modifica CreateProductEndpoint
    Adiciona referências aos pacotes Carter (v8.0.0) e Mapster (v7.4.0) no arquivo `BuildingBlocks.csproj`.
    
    Altera `CreateProductEndpoint.cs` para incluir `using Carter;`, implementa `ICarterModule`, adiciona registros `CreateProductRequest` e `CreateProductResponse`, e adiciona método `AddRoutes` vazio.
    
    Remove `using MediatR;` de `CreateProductHandler.cs`.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,14 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndpoint
+    public record CreateProductRequest(string Name, string Description, decimal Price, string ImageFileName, string CategoryName);
+
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+        }
     }
 }
