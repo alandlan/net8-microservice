6df718560be2fb5eb39830d0c85d8a4ff277e3a5
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-11 15:28:03 -0300

    Adiciona Marten e ajusta manipulação de produtos
    - Atualiza `Catalog.API.csproj` para incluir o pacote `Marten` versão `6.4.1`.
    - Adiciona diretivas `global using` em `GlobalUsing.cs` para `BuildingBlocks.CQRS`, `Catalog.API.Models` e `Marten`; reorganiza `global using MediatR`.
    - Modifica `CreateProductHandler` para aceitar `IDocumentSession` no construtor.
    - Altera método `Handle` para armazenar produto na sessão `Marten` e salvar alterações de forma assíncrona.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,25 +1,24 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductHandler 
+    internal class CreateProductHandler(IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             var product = new Product
             {
-                Id = Guid.NewGuid(),
                 Name = command.Name,
                 Category = command.Category,
                 Description = command.Description,
                 ImageFile = command.ImageFile,
                 Price = command.Price
             };
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
 
             return new CreateProductResult(product.Id);
         }
