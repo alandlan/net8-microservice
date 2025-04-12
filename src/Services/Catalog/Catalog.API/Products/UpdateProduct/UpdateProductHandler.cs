9fa0caff79d78e8e2b78971d00fd1b7297107726
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-12 13:49:39 -0300

    Adiciona endpoint e handler para atualização de produtos
    Foram adicionados os arquivos `UpdateProductEndpoint.cs` e `UpdateProductHandler.cs` para implementar a funcionalidade de atualização de produtos na API do catálogo.
    
    No arquivo `UpdateProductEndpoint.cs`:
    - Criado o namespace `Catalog.API.Products.UpdateProduct`.
    - Definidos os registros `UpdateProductRequest` e `UpdateProductResponse`.
    - Criada a classe `UpdateProductEndpoint` que implementa `ICarterModule` e define a rota HTTP PUT `/products`.
    - A rota mapeada utiliza um comando adaptado de `UpdateProductRequest` para `UpdateProductCommand` e envia esse comando usando um `ISender`. A resposta é adaptada para `UpdateProductResponse` e retornada com o status HTTP 200 OK.
    
    No arquivo `UpdateProductHandler.cs`:
    - Criado o namespace `Catalog.API.Products.UpdateProduct`.
    - Definido o registro `UpdateProductCommand`.
    - Definida a classe `UpdateProductResult`.
    - Criada a classe `UpdateProductHandler` que implementa `ICommandHandler<UpdateProductCommand, UpdateProductResult>`.
    - O método `Handle` carrega o produto pelo ID, atualiza suas propriedades e salva as mudanças no banco de dados. Se o produto não for encontrado, uma exceção `ProductNotFoundException` é lançada.

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name,List<string> Category, string Description, string ImageUrl, decimal Price) 
+        : ICommand<UpdateProductResult>;
+
+    public class UpdateProductResult(bool IsSuccess);
+    internal class UpdateProductHandler 
+        (IDocumentSession session, ILogger<UpdateProductHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductHandler.Handle called with {@Command}", command);
+
+            var product = await session.LoadAsync<Product>(command.Id);
+
+            if (product is null)
+                throw new ProductNotFoundException();
+            
+            product.Name = command.Name;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageUrl;
+            product.Price = command.Price;
+            product.Category = command.Category;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    }
+}
