309931f28a99f2e56627ea5d889c2460c301779e
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-12 14:00:01 -0300

    Adiciona endpoint para deletar produtos na API de catálogo
    Foram adicionadas novas funcionalidades para deletar produtos na API de catálogo. No arquivo `DeleteProductEndpoint.cs`, foi criado um novo endpoint para deletar produtos, mapeado para a rota `/products/{id:guid}` e utilizando o método HTTP DELETE. No arquivo `DeleteProductHandler.cs`, foi adicionado um novo comando `DeleteProductCommand` e um handler `DeleteProductHandler` para gerenciar a deleção de produtos do banco de dados.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductResponse(bool isSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id:guid}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id);
+
+                var response = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete a product")
+            .WithDescription("Delete a product by its ID");
+        }
+    }
+}
