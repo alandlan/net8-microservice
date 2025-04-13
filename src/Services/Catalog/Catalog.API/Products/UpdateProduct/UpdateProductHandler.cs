5affd327323b464b46c34d60d2af67ad99f03847
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-13 09:43:20 -0300

    Adiciona validadores para comandos de produto
    Adição de `DeleteProductCommandValidator` e `UpdateProductCommandValidator` para validar os comandos `DeleteProductCommand` e `UpdateProductCommand`, respectivamente. Assegura que campos obrigatórios sejam preenchidos e valores atendam a condições específicas antes do processamento. Mensagens de erro apropriadas são exibidas em caso de falha na validação.

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -6,6 +6,20 @@ namespace Catalog.API.Products.UpdateProduct
         : ICommand<UpdateProductResult>;
 
     public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required.");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("ImageUrl is required.");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
+        }
+    }
+
     internal class UpdateProductHandler 
         (IDocumentSession session, ILogger<UpdateProductHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
