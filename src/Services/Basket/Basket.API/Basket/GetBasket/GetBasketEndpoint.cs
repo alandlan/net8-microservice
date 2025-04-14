dfbef08d4430f0c9091727b285c712c61f69fa1b
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-14 14:16:05 -0300

    Refatoração e adição de dependências
    No arquivo `Basket.API.csproj`, foi adicionada uma referência ao pacote `Carter` na versão `8.0.0` e um caractere invisível foi adicionado no início da linha `<Project Sdk="Microsoft.NET.Sdk.Web">`.
    
    No arquivo `GetBasketEndpoint.cs`, a classe `GetBasketEndpoint` foi modificada para ser um `record` chamado `GetBasketResponse` que contém um `ShoppingCart`. A classe `GetBasketEndpoint` agora implementa a interface `ICarterModule` e define uma rota GET para `/basket/{username}` que retorna um `GetBasketResponse`.
    
    No arquivo `GlobalUsing.cs`, foram adicionadas referências globais para os namespaces `Carter`, `MediatR` e `Mapster`.

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,6 +1,25 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketEndpoint
+    public record GetBasketResponse(ShoppingCart Cart);
+    public class GetBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{username}", async (string userName, ISender sender) =>
+            {
+                var query = new GetBasketQuery(userName);
+
+                var result = await sender.Send(query);
+
+                var response = result.Adapt<GetBasketResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetBasket")
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get a user's shopping cart")
+            .WithDescription("Get a user's shopping cart by username");
+        }
     }
 }
