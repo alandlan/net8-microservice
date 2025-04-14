087e0d20060db3a9602e07ff485c20c611b8e235
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-14 14:38:46 -0300

    Implementa endpoint DELETE para deletar basket por userName
    - `DeleteBasketEndpoint` alterado para registro `DeleteBasketResponse` com propriedade `IsSuccess`.
    - `DeleteBasketEndpoint` agora implementa `ICarterModule`.
    - Adicionado método `AddRoutes` em `DeleteBasketEndpoint`.
    - Mapeada rota HTTP DELETE para `/basket/{userName}`.
    - Rota DELETE usa `DeleteBasketCommand` para deletar basket por `userName`.
    - Resultado adaptado para `DeleteBasketResponse` e retornado com status 200 OK.
    - Rota pode retornar status 400 Bad Request e 404 Not Found.
    - Adicionadas descrições e resumos para a rota.

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,24 @@
 ﻿namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketEndpoint
+    public record DeleteBasketResponse(bool IsSuccess);
+    public class DeleteBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var command = await sender.Send(new DeleteBasketCommand(userName));
+
+                var result = command.Adapt<DeleteBasketResponse>();
+
+                return Results.Ok(result);
+            })
+                .WithName("DeleteBasket")
+                .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status400BadRequest)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithSummary("Delete a user's basket")
+                .WithDescription("Delete a user's basket by their username.");
+        }
     }
 }
