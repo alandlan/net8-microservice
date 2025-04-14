14ea83844912bcd3a976c909d65ff30db86fc70f
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-14 14:42:36 -0300

    Adiciona Carter e comportamentos de logging/validação
    - Adiciona global using `BuildingBlocks.Behaviors` em `GlobalUsing.cs`
    - Adiciona serviço Carter ao container de serviços em `Program.cs`
    - Configura MediatR para registrar serviços do assembly atual
    - Adiciona comportamentos de logging e validação ao MediatR
    - Mapeia Carter no pipeline de requisições HTTP

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(assembly);
+    cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
