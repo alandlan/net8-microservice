5d15e23a5dab65115f7c6b05a13474e1002a0d0f
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-17 14:30:25 -0300

    Integração do EF Core com SQLite no serviço gRPC
    Foram realizadas as seguintes alterações para integrar o Entity Framework Core com o banco de dados SQLite no projeto gRPC:
    
    - Adicionadas as dependências `Microsoft.EntityFrameworkCore.Sqlite` e `Microsoft.EntityFrameworkCore.Tools` no arquivo `Discount.Grpc.csproj`.
    - Configurado o contexto de banco de dados `DiscountContext` no arquivo `Program.cs` para usar SQLite com a string de conexão definida no `appsettings.json`.
    - Adicionada a string de conexão para o banco de dados SQLite no arquivo `appsettings.json`.
    - Criada a classe `DiscountContext` para gerenciar o contexto do banco de dados e mapear a entidade `Coupon` para a tabela `Coupons`.
    - Gerada a migração inicial para criar a tabela `Coupons` no banco de dados, incluindo os arquivos relacionados à migração (`20250417172822_Initial-Create.cs`, `.Designer.cs` e `DiscountContextModelSnapshot.cs`).
    
    Essas mudanças permitem a persistência de dados no banco SQLite, integrando o EF Core ao serviço gRPC.

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+        }
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasKey(x => x.Id);
+            modelBuilder.Entity<Coupon>().Property(x => x.ProductName).IsRequired();
+            modelBuilder.Entity<Coupon>().Property(x => x.Description).IsRequired();
+            modelBuilder.Entity<Coupon>().Property(x => x.Amount).IsRequired();
+            modelBuilder.Entity<Coupon>().ToTable("Coupons");
+        }
+    }
+}
