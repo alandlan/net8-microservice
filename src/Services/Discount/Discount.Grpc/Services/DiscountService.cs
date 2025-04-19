1d8669a25cc8c68f0f29cf2a421fe65cc819f42d
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-19 12:50:58 -0300

    Torna CreateDiscount assíncrono e adiciona validações
    - Alteração do método `CreateDiscount` para assíncrono,
      melhorando escalabilidade e desempenho.
    - Adicionada validação para garantir que o objeto `Coupon`
      não seja nulo, lançando `RpcException` em caso de erro.
    - Mapeamento do objeto `Coupon` para o modelo `Models.Coupon`
      com o método `Adapt`.
    - Persistência do objeto `Coupon` no banco de dados com
      `dbContext.Coupons.Add` e `SaveChangesAsync`.
    - Adicionado log informativo para rastrear criação de descontos.
    - Retorno do modelo `CouponModel` como resposta.
    - Alterações nos arquivos binários do SQLite devido a mudanças
      no banco de dados.

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -22,9 +22,20 @@ namespace Discount.Grpc.Services
             return couponModel;
         }
 
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
         {
-            return base.CreateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Models.Coupon>();
+
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object."));
+
+            dbContext.Coupons.Add(coupon);
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount is successfully created. ProductName: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
         }
 
         public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
