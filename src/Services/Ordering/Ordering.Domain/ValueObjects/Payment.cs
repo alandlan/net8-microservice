012cc2186d02ef6fe6b45ae88480a351b4920648
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-23 14:44:48 -0300

    feat: Adiciona modelos e enums para domínio de pedidos
    - Adicionado `global using` no arquivo `GlobalUsing.cs` para incluir os namespaces `Ordering.Domain.Abstractions`, `Ordering.Domain.Enums` e `Ordering.Domain.ValueObjects`.
    - Criado o enum `OrderStatus` para representar estados de pedidos: `Draft`, `Pending`, `Completed` e `Cancelled`.
    - Criadas as classes `Customer`, `Order`, `OrderItem` e `Product` no namespace `Ordering.Domain.Models`, representando entidades do domínio de pedidos.
    - Adicionados os records `Address` e `Payment` no namespace `Ordering.Domain.ValueObjects` para representar objetos de valor relacionados a endereços e pagamentos.

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Payment
+    {
+        public string? CardName { get; init; } = default!;
+        public string CardNumber { get; init; } = default!;
+        public string Expiration { get; init; } = default!;
+        public string Cvv { get; init; } = default!;
+        public decimal PaymentMethod { get; init; } = default!;
+    }
+}
