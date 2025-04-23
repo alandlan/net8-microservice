f724aaaba3550401719a9ab3e33512ae3322249c
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-23 15:12:46 -0300

    Refatoração e melhorias no domínio de Ordering
    - Adicionada declaração global `Ordering.Domain.Exceptions` no `GlobalUsing.cs`.
    - Implementado método estático `Create` na classe `Customer` para criação controlada de instâncias com validação de `name` e `email`.
    - Alterada a classe `CustomerId` para incluir construtor privado e método estático `Of` com validação de `Guid`.
    - Criada a classe `DomainException` para gestão de erros no domínio.

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base($"Domain Exception: \"{message}\" throws from Domain Layer.")
+        {
+        }
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+        public DomainException(string message, string paramName) : base($"{message} (Parameter '{paramName}')")
+        {
+        }
+        public DomainException(string message, string paramName, Exception innerException) : base($"{message} (Parameter '{paramName}')", innerException)
+        {
+        }
+    }
+}
