11212fa540ff0d3fb30e1c37ad1e26b65c31da01
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-28 13:48:49 -0300

    Adiciona seed inicial para tabela Customers no banco
    Introduz método `SeedAsync` em `DatabaseExtensions.cs` para
    popular a tabela `Customers` após migrações. Cria o arquivo
    `InitialData.cs` com a classe `InitialData`, que fornece uma
    lista estática de clientes pré-definidos. Adiciona dois
    registros iniciais de clientes com IDs, nomes e e-mails fixos.
    Importa namespaces necessários para suportar o domínio.

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtentions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtentions.cs
@@ -18,6 +18,22 @@ namespace Ordering.Infrastructure.Data.Extensions
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
             context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+            await SeedAsync(context);
+        }
+
+        private static async Task SeedAsync(ApplicationDbContext context)
+        {
+            await SeedCustomerAsync(context);
+        }
+
+        private static async Task SeedCustomerAsync(ApplicationDbContext context)
+        {
+            if(!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Extensions
+{
+    public class InitialData
+    {
+        public static IEnumerable<Customer> Customers => new List<Customer>
+        {
+            Customer.Create(CustomerId.Of(new Guid("E3678D94-F208-4172-997E-CBCE78D3B990")),"User 1", "user1@email.com"),
+            Customer.Create(CustomerId.Of(new Guid("044F58EF-1A54-4B10-9AAA-D4175C5B5136")),"User 2", "user2@email.com"),
+        };
+    }
+}
